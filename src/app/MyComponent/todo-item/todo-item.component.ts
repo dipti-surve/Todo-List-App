8e4fddd765d21cb40b1d405e172b89deac62519e
Dipti Surve <diptisurve7415@gmmail.com>
2024-03-06 13:59:59 +0530

    All functionalities added

--- src/app/MyComponent/todo-item/todo-item.component.ts
+++ src/app/MyComponent/todo-item/todo-item.component.ts
@@ -7,7 +7,9 @@ import { Todo } from 'src/app/Todo';
 })
 export class TodoItemComponent implements OnInit{
   @Input() todo!: Todo;
+  @Input() i!:number;
   @Output() todoDelete: EventEmitter<Todo> = new EventEmitter();
+  @Output() todoCheckbox: EventEmitter<Todo> = new EventEmitter();
   constructor(){ }
   ngOnInit(): void {   
   }
@@ -15,7 +17,7 @@ export class TodoItemComponent implements OnInit{
     this.todoDelete.emit(todo);
     console.log("onClick has been triggered")
   }
-  onCheckboxClick(todo:Todo){
-
+  onCheckboxClick(todo:any){
+    this.todoCheckbox.emit(todo);
   }
 }
--- src/app/MyComponent/todos/todos.component.ts
+++ src/app/MyComponent/todos/todos.component.ts
@@ -35,6 +35,11 @@ export class TodosComponent implements OnInit{
     this.todos.push(todo);
     localStorage.setItem("todos" , JSON.stringify(this.todos));
   }
+  toggleTodo(todo:any){
+    const index = this.todos.indexOf(todo);
+    this.todos[index].active = !this.todos[index].active;
+    localStorage.setItem("todos" , JSON.stringify(this.todos));
+  }
 }
 
 export { Todo };
